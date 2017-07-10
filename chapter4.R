1636686a5dae129a09a3724b893fbf43986bc5c4
josephscheidt <josephjscheidt@gmail.com>
2017-07-10 00:24:38 -0700

    Finished chapter 4.

---
+++ chapter4.R
@@ -0,0 +1,8 @@
+##Workflow: Scripts
+
+#1. With Control F and query, you can select all and edit each instance of a
+#word. Which is great for changing object names.
+
+#2. Check arguments to R function calls, Warn if variable used has no definition
+#in scope, Warn if variable is defined but not used, Provide R style diagnostics
+#(e.g. whitespace)
